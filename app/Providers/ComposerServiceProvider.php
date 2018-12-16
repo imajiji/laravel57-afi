f9ee91cfb6389a12579bde941fd38e9359e2ddb4
vagrant <vagrant@localhost.localdomain>
2018-12-16 10:46:24 -0500

    Add ComposerServiceProvider

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Category;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer('layouts.header', function($view){
+            $categories = Category::all();
+ 
+            return $view->with('categories', $categories);
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
